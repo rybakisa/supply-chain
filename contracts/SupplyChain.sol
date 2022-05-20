9a5fde338489dec9bf363070099a59f5f01eacae
Andrey Ezhov <rybakisa@gmail.com>
2022-05-20 18:35:16 +0400

    Refactor items datastructures
    - Replace items mapping with items array
    - Create mappings for tracking items ownership
    - Refactor weird items' ids incrementation
    - Beautify state changer functions

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -7,15 +7,17 @@ contract SupplyChain {
     enum ItemState {Created, Paid, Delivered}
     
     struct Item {
-        SupplyChain.ItemState state;
         string name;
         uint priceInWei;
+        SupplyChain.ItemState state;
     }
     
-    mapping(uint => Item) public items;
-    uint itemId;
+    Item[] public items;
+
+    mapping (uint => address) public itemToOwner;
+    mapping (address => uint) ownerItemsCount;
     
-    event ItemStateChanged(uint _itemId, uint _state);
+    event ItemStateChanged(uint  _itemId, uint _state);
 
     modifier itemHasState(uint _itemId, SupplyChain.ItemState _state) {
         require(items[_itemId].state == _state, "Item is further in the supply chain");
@@ -23,26 +25,31 @@ contract SupplyChain {
     }
 
     function createItem(string memory _name, uint _priceInWei) public {
-        items[itemId].priceInWei = _priceInWei;
-        items[itemId].state = ItemState.Created;
-        items[itemId].name = _name;
+        ItemState newState = ItemState.Created;
+        items.push(
+            Item(_name, _priceInWei, newState)
+        );
+        uint itemId = items.length - 1;
+        
+        itemToOwner[itemId] = msg.sender;
+        ownerItemsCount[msg.sender]++;
 
         emit ItemStateChanged(itemId, uint(items[itemId].state));
-        
-        itemId++;
     }
 
     function receivePayment(uint _itemId) public payable itemHasState(_itemId, ItemState.Created) {
         require(items[_itemId].priceInWei <= msg.value, "Not fully paid");
         
-        items[_itemId].state = ItemState.Paid;
+        ItemState newState = ItemState.Paid;
+        items[_itemId].state = newState;
         
-        emit ItemStateChanged(_itemId, uint(items[_itemId].state));
+        emit ItemStateChanged(_itemId, uint(newState));
     }
 
     function deliverItem(uint _itemId) public itemHasState(_itemId, ItemState.Paid) {
-        items[_itemId].state = ItemState.Delivered;
+        ItemState newState = ItemState.Delivered;
+        items[_itemId].state = newState;
         
-        emit ItemStateChanged(_itemId, uint(items[_itemId].state));
+        emit ItemStateChanged(_itemId, uint(newState));
     }
 }
